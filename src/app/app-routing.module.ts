ca681a721bb3e1aa2b7ba1dcd79b6f2c7ebde804
lyoman <leomuchenje@gmail.com>
2021-02-23 12:42:34 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,53 @@
+import { AuthGuard } from './guard/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: "",
+    loadChildren: () => import('./auth/login/login.module').then((m) => m.LoginModule),
+    pathMatch: "full"
+  },
+  {
+    path: '',
+    loadChildren: () => import('./layout/layout.module').then((m) => m.LayoutModule),
+    canActivate: [AuthGuard]
+  },
+
+  {
+    path: 'patient',
+    loadChildren: () => import('./users/patient/patient.module').then((m) => m.PatientModule),
+    canActivate: [AuthGuard]
+  },
+
+  {
+    path: 'nurse',
+    loadChildren: () => import('./users/nurse/nurse.module').then((m) => m.NurseModule),
+    canActivate: [AuthGuard]
+  },
+
+  {
+    path: 'doctor',
+    loadChildren: () => import('./users/doctor/doctor.module').then((m) => m.DoctorModule),
+    canActivate: [AuthGuard]
+  },
+
+  { path: 'login', loadChildren: () => import('./auth/login/login.module').then((m) => m.LoginModule) },
+
+  // { path: 'register', loadChildren: () => import('./auth/register/register.module').then((m) => m.RegisterModule) },
+
+  // {
+  //   path: 'error',
+  //   loadChildren: () => import('./server-error/server-error.module').then((m) => m.ServerErrorModule)
+  // },
+  // {
+  //   path: 'access-denied',
+  //   loadChildren: () => import('./access-denied/access-denied.module').then((m) => m.AccessDeniedModule)
+  // },
+  // { path: 'not-found', loadChildren: () => import('./not-found/not-found.module').then((m) => m.NotFoundModule) },
+  // { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
+import { AuthGuard } from './guard/auth.guard';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
+
+import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MbscModule } from '@mobiscroll/angular-lite';
+// import { RegisterComponent } from './auth/register/register.component';
+import { PatientComponent } from './users/patient/patient.component';
+// import { CustomFormsModule } from 'ngx-custom-validators';
+// import  {  PdfViewerModule  }  from  'ng2-pdf-viewer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // RegisterComponent,
+    PatientComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    HttpClientModule,
+    // CustomFormsModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot(),
+    FormsModule, 
+    MbscModule,
+    MatSidenavModule,
+    MatDividerModule,
+    // PdfViewerModule
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
